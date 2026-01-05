550c98c08b89b2fe2e0b3e6806551f95dca335a7
youssef-tarek-abdelaziz <youssef.tarek.abdelaziz@gmail.com>
2026-01-05 14:02:08 +0200

    remove gradle, update texttest_rig.py

--- Java/texttest_rig.py
+++ Java/texttest_rig.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """
-This script uses Gradle to execute the TexttestFixture.
+This script uses Maven to execute the TexttestFixture.
 It is designed to be used by TextTest and specified in the file 'texttests/config.gr' in this repo.
-It is more convenient for TextTest to use since Gradle needs
+It is more convenient for TextTest to use since Maven needs
 several arguments in addition to the one the TextTest fixture needs.
 """
 import os
@@ -11,4 +11,10 @@ import sys
 
 args = " ".join(sys.argv[1:])
 TEXTTEST_HOME = os.environ.get("TEXTTEST_HOME", os.getcwd())
-subprocess.run(f"{TEXTTEST_HOME}/Java/gradlew -p {TEXTTEST_HOME}/Java -q text --args {args}", shell=True)
+java_dir = os.path.join(TEXTTEST_HOME, "")
+
+subprocess.run(
+    f'./mvnw -q test-compile exec:java -Dexec.mainClass="com.gildedrose.TexttestFixture" -Dexec.args="{args}"',
+    shell=True,
+    cwd=java_dir
+)
