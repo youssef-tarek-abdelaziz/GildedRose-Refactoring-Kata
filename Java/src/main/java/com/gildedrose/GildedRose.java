4fccfacdcf262a55c6982e138647757cd1f4b85c
youssef-tarek-abdelaziz <youssef.tarek.abdelaziz@gmail.com>
2026-01-05 17:11:08 +0200

    scale statements

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,6 +13,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+            if(isUnchangableItem(i))
+                continue;
             if (items[i].name.equals(AGED_BRIE) || items[i].name.equals(BACKSTAGE)) {
                 if (isLessThanMaxQuality(i)) {
                     increaseQuality(i);
@@ -33,15 +35,11 @@ class GildedRose {
                 }
             } else {
                 if (isQualityGreaterThanZero(i)) {
-                    if (!isUnchangableItem(i)) {
-                        decreaseQuality(i);
-                    }
+                    decreaseQuality(i);
                 }
             }
 
-            if (!isUnchangableItem(i)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            items[i].sellIn = items[i].sellIn - 1;
 
             if (items[i].sellIn < 0) {
                 if (items[i].name.equals(AGED_BRIE)) {
@@ -53,9 +51,7 @@ class GildedRose {
                         items[i].quality = 0;
                     } else {
                         if (isQualityGreaterThanZero(i)) {
-                            if (!isUnchangableItem(i)) {
-                                decreaseQuality(i);
-                            }
+                            decreaseQuality(i);
                         }
                     }
                 }
